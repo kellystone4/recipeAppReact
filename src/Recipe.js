c4042e29e3629b4d7586db818da68dc4cb3570b7
Kelly Stone <kellystone916@gmail.com>
2021-03-10 15:26:25 -0800

    fix stylization

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,11 +1,13 @@
 import React from "react";
+import style from "./recipe.module.css"
 
 const Recipe = ({title, calories,image, ingredients}) => {
   return (
-    <div>
+    <div className={style.recipe}>
       <h1>{title}</h1>
-      <p>{calories}</p>
-      <img src={image} alt="" />
+      <p>Calories: {calories}</p>
+      <img className={style.image}src={image} alt="" />
+      <p>Ingredients:</p>
       <ol>
         {ingredients.map(ingredient => (
         <li>{ingredient.text}</li>
